0737baecdf2b887e2450fa62b8d818a9654ecbd5
sukumarAML <147479940+sukumarAML@users.noreply.github.com>
2026-09-24 12:46:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Display current year automatically
 
+document.getElementById("year").textContent =
+    new Date().getFullYear();
+
+
+// Project button
+
+function showProject(projectName) {
+
+    alert(
+        "You selected: " +
+        projectName +
+        "\n\nProject details can be added here."
+    );
+
+}
